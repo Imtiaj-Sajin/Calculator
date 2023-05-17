3d36e24356d267c5823667b13de09132b3f4c043
Imtiaj Sajin <imtiajsajin@gmail.com>
2023-05-17 17:21:22 +0600

    animations added

--- FormBg.Designer.cs
+++ FormBg.Designer.cs
@@ -33,12 +33,14 @@
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBg));
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.buttonMinimize = new Guna.UI2.WinForms.Guna2ImageButton();
             this.guna2ImageButtonClose = new Guna.UI2.WinForms.Guna2ImageButton();
             this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
             this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,10 +48,10 @@
             // 
             this.guna2Panel1.CustomizableEdges = customizableEdges1;
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.guna2Panel1.Location = new System.Drawing.Point(0, 13);
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 12);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            this.guna2Panel1.Size = new System.Drawing.Size(260, 427);
+            this.guna2Panel1.Size = new System.Drawing.Size(254, 433);
             this.guna2Panel1.TabIndex = 0;
             // 
             // panel1
@@ -61,7 +63,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
-            this.panel1.Size = new System.Drawing.Size(260, 13);
+            this.panel1.Size = new System.Drawing.Size(254, 12);
             this.panel1.TabIndex = 1;
             // 
             // buttonMinimize
@@ -77,12 +79,14 @@
             this.buttonMinimize.ImageOffset = new System.Drawing.Point(0, 0);
             this.buttonMinimize.ImageRotate = 0F;
             this.buttonMinimize.ImageSize = new System.Drawing.Size(10, 10);
-            this.buttonMinimize.Location = new System.Drawing.Point(222, 0);
+            this.buttonMinimize.Location = new System.Drawing.Point(216, 0);
             this.buttonMinimize.Name = "buttonMinimize";
-            this.buttonMinimize.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.buttonMinimize.PressedState.Image = global::Calculator_Suva.Properties.Resources.mini;
+            this.buttonMinimize.PressedState.ImageSize = new System.Drawing.Size(10, 10);
             this.buttonMinimize.ShadowDecoration.CustomizableEdges = customizableEdges3;
-            this.buttonMinimize.Size = new System.Drawing.Size(13, 13);
+            this.buttonMinimize.Size = new System.Drawing.Size(13, 12);
             this.buttonMinimize.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.buttonMinimize, "Minimize");
             this.buttonMinimize.UseTransparentBackground = true;
             this.buttonMinimize.Click += new System.EventHandler(this.buttonMinimize_Click);
             // 
@@ -98,12 +102,14 @@
             this.guna2ImageButtonClose.ImageOffset = new System.Drawing.Point(0, 0);
             this.guna2ImageButtonClose.ImageRotate = 0F;
             this.guna2ImageButtonClose.ImageSize = new System.Drawing.Size(10, 10);
-            this.guna2ImageButtonClose.Location = new System.Drawing.Point(235, 0);
+            this.guna2ImageButtonClose.Location = new System.Drawing.Point(229, 0);
             this.guna2ImageButtonClose.Name = "guna2ImageButtonClose";
-            this.guna2ImageButtonClose.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButtonClose.PressedState.Image = global::Calculator_Suva.Properties.Resources.macClose;
+            this.guna2ImageButtonClose.PressedState.ImageSize = new System.Drawing.Size(10, 10);
             this.guna2ImageButtonClose.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            this.guna2ImageButtonClose.Size = new System.Drawing.Size(13, 13);
+            this.guna2ImageButtonClose.Size = new System.Drawing.Size(13, 12);
             this.guna2ImageButtonClose.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.guna2ImageButtonClose, "Close");
             this.guna2ImageButtonClose.UseTransparentBackground = true;
             this.guna2ImageButtonClose.Click += new System.EventHandler(this.buttonClose_Click);
             // 
@@ -124,11 +130,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(260, 440);
+            this.ClientSize = new System.Drawing.Size(254, 445);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormBg";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormBg";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -143,5 +151,6 @@
         private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
         private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButtonClose;
         private Guna.UI2.WinForms.Guna2ImageButton buttonMinimize;
+        private ToolTip toolTip1;
     }
 }
--- FormBg.cs
+++ FormBg.cs
@@ -15,6 +15,17 @@ namespace Calculator
 
     public partial class FormBg : Form
     {
+
+        public const int AW_HOR_POSITIVE = 0x10000;
+        public const int AW_SLIDE = 0x40000;
+        public const int AW_HIDE = 0x10000;
+        public const int AW_VER_NEGATIVE = 0x0004;
+        public const int AW_VER_POSITIVE = 0x0008;
+        public const int AW_BLEND = 0x00070000;
+
+
+        [DllImport("user32.dll")]
+        static extern bool AnimateWindow(IntPtr hWnd, int dwTime, int dwFlags);              
         Form activeForm = null;
 
         public FormBg()
@@ -64,12 +75,23 @@ namespace Calculator
 
         private void buttonClose_Click(object sender, EventArgs e)
         {
+            AnimateWindow(this.Handle, 100,  AW_VER_POSITIVE| AW_SLIDE |AW_HIDE); 
             Application.Exit();
         }
 
         private void buttonMinimize_Click(object sender, EventArgs e)
         {
+            // Animate the form out
+            AnimateWindow(this.Handle, 100, AW_VER_NEGATIVE | AW_SLIDE | AW_BLEND);
+
+            // Minimize the form
             this.WindowState = FormWindowState.Minimized;
+
+            // Show the form on the taskbar
+            //this.ShowInTaskbar = true;
+
+           
         }
+
     }
 }
